42bb6b043bdb1d1e8e24b875a88f2f23579f4ea2
Andreas Wolke <wolke@vmbichler5.informatik.tu-muenchen.de>
2014-01-02 01:48:08 +0100

    Documentation

--- LinkedCircleEfficient.java
+++ LinkedCircleEfficient.java
@@ -23,6 +23,28 @@ public class LinkedCircleEfficient {
             System.out.println("Circle root: " + root.data); 
     }
 
+    /**
+     * Slow pointer moves one elements and the fast one two
+     * After k elements slow pointer moves circle start s
+     * Faster poinster has moved 2k elements
+     * Of the 2k elements 2k - k = k elements in cycle
+     * Fast pointer has cycle position F = k % S (S elements in cycle)
+     * Faster pointer is S - F elements behind slow pointer and catches up with one element per unit of time
+     * After S - F additional steps slow and fast pointer collide
+     * So collission happens S - F steps after element or F steps before start
+     * --- collision detected -> cycle detected -> where is cycle start?
+     * From collission F elements to start
+     * F = k % S -> k = F + M * S (with M natural positive number)
+     * Because of cycle M * S can be ignored
+     * Moving both pointers at normal speed (one element at a unit of time) for k
+     * elements moves fast pointer to cycle start
+     * We know that slow pointer moves k elements from list start to cycle start
+     * So, slow pointer is set to list start and fast pointer is left as it is
+     * both pointers are moved until they collade
+     * Fast pointer has moved F steps equal k steps and slow pointer has moved k steps
+     * as well
+     * Hence, the collission happens at the cycle start. 
+     */
     Linked findCircleRoot(Linked linked) {
         // Use slow and fast pointer to go through linked list
         Linked slow = linked;
